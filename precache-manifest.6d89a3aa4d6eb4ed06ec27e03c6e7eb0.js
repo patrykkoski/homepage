ef5dcad26eaa3ae608df2dd3bc20666c63f50045
patrykkoski <patrykkoski1994@gmail.com>
2019-02-18 21:45:23 +0100

    Updates

---
+++ precache-manifest.6d89a3aa4d6eb4ed06ec27e03c6e7eb0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "31f337777a73dc455037978deb9eba49",
+    "url": "/static/media/automatify.31f33777.png"
+  },
+  {
+    "revision": "e4bf47cdf2ee549d7be7",
+    "url": "/static/css/main.265c2d02.chunk.css"
+  },
+  {
+    "revision": "e862d2dd56eef653b8de",
+    "url": "/static/js/1.e862d2dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "faa4bd1d5266713679a51776fb5bb7ce",
+    "url": "/static/media/bg.faa4bd1d.png"
+  },
+  {
+    "revision": "869cc21d68ec33ed42e948e556a62bb4",
+    "url": "/static/media/about.869cc21d.jpg"
+  },
+  {
+    "revision": "e4bf47cdf2ee549d7be7",
+    "url": "/static/js/main.e4bf47cd.chunk.js"
+  },
+  {
+    "revision": "505251df7a4983df331f3291b0378f91",
+    "url": "/static/media/team.505251df.png"
+  },
+  {
+    "revision": "738c1c8f96db75d546cd6553c53a58de",
+    "url": "/static/media/test.738c1c8f.png"
+  },
+  {
+    "revision": "5413331051120157ac25b566e7c5a825",
+    "url": "/static/media/contact.54133310.png"
+  },
+  {
+    "revision": "686b5d4a9c894932f34b38d67f7f0f31",
+    "url": "/static/media/li.686b5d4a.png"
+  },
+  {
+    "revision": "ea87552b405a358acac2f425d688cd29",
+    "url": "/index.html"
+  }
+];
