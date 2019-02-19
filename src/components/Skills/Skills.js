dee75a000bfefbe8e798764e9458ab14968ec542
patrykkoski <patrykkoski1994@gmail.com>
2019-02-19 21:13:44 +0100

    initial commit

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,33 +4,64 @@ import "./Skills.css";
 const skills = () => {
   return (
     <div className="skills">
-    <div className="changePageScreen">
+      <div className="changePageScreen">
         <img src={require("../../dist/img/logo.png")} alt="logo" />
         <div className="page-loader" />
       </div>
+      <h2>Technologies that I use</h2>
       <div className="skills-wraper">
         <div className="tech-description">
-          <img src={require('../../dist/img/skills/html.png')} width="200" height="200" alt="html" />
+          <img
+            src={require("../../dist/img/skills/html.png")}
+            width="180"
+            height="180"
+            alt="html"
+          />
           <h3>HTML5</h3>
         </div>
         <div className="tech-description">
-        <img src={require('../../dist/img/skills/css.png')} width="200" height="200" alt="css" />
+          <img
+            src={require("../../dist/img/skills/css.png")}
+            width="180"
+            height="180"
+            alt="css"
+          />
           <h3>CSS3</h3>
         </div>
         <div className="tech-description">
-        <img src={require('../../dist/img/skills/js.png')} width="200" height="200" alt="js" />
+          <img
+            src={require("../../dist/img/skills/js.png")}
+            width="180"
+            height="180"
+            alt="js"
+          />
           <h3>JavaScript</h3>
         </div>
         <div className="tech-description">
-        <img src={require('../../dist/img/skills/react.png')} width="200" height="200" alt="react" />
+          <img
+            src={require("../../dist/img/skills/react.png")}
+            width="180"
+            height="180"
+            alt="react"
+          />
           <h3>ReactJS</h3>
         </div>
         <div className="tech-description">
-        <img src={require('../../dist/img/skills/bootstrap.png')} width="200" height="200" alt="bootstrap" />
+          <img
+            src={require("../../dist/img/skills/bootstrap.png")}
+            width="180"
+            height="180"
+            alt="bootstrap"
+          />
           <h3>Bootstrap</h3>
         </div>
         <div className="tech-description">
-        <img src={require('../../dist/img/skills/sass.png')} width="200" height="200" alt="sass" />
+          <img
+            src={require("../../dist/img/skills/sass.png")}
+            width="180"
+            height="180"
+            alt="sass"
+          />
           <h3>Sass</h3>
         </div>
       </div>
